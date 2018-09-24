6fbcc8eba455b68365c4c2263fd20d7c6cb7e47f
xuxu1992 <402958518@qq.com>
2018-09-24 22:20:40 +0800

    cpp version

---
+++ CAS_FUNC.hpp
@@ -0,0 +1,15 @@
+#ifndef __CAS_FUNC__
+#define __CAS_FUNC__
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include <stdbool.h>
+
+    bool DO_CAS(int * ptr, int oldval, int newval);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
