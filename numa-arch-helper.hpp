55ba0c41a2f9fcc87c62e6f0f683aefd111d4833
xuxu1992 <402958518@qq.com>
2018-09-24 22:21:11 +0800

    multiple core funtions in numa arch

---
+++ numa-arch-helper.hpp
@@ -0,0 +1,16 @@
+#ifdef __linux__
+#include <thread>
+
+
+/**
+ * init numa architechture cpu layout
+*/
+int init_numa_cpu_env();
+
+/**
+ * migrate target thread to a cpu which is in the same socket node as process who create this thread 
+*/
+int thread_migrate(std::thread & m_thread);
+
+
+#endif
