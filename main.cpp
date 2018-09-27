c2cb2838460895a33e8d9c48baa4d27bec473ae2
xk <402958518@qq.com>
2018-09-27 16:19:16 +0800

    infinite loop threads working mode test

--- main.cpp
+++ main.cpp
@@ -9,12 +9,14 @@
 #include <iostream>
 #include <thread>
 #include <functional>
+#include <signal.h>
+#include <inttypes.h>
 
 #include <CAS_FUNC.hpp>
 #include <numa-arch-helper.hpp>
 
 #define DATA_NB 1024
-#define MAX_THD_NB 2
+#define MAX_THD_NB 8
 
 //functions
 void test_data_init();
@@ -22,6 +24,8 @@ void env_init();
 void env_uinit();
 
 //thread var
+int force_quit;
+uint8_t self_lock;
 pthread_cond_t cond;
 pthread_mutex_t cond_mutex;
 std::thread * thds[MAX_THD_NB];
@@ -40,8 +44,10 @@ shared_data test_data;
 void env_init()
 {
     init_numa_cpu_env();
-    
+
     test_data_init();
+
+    self_lock = 0xFF;
 }
 
 void env_uinit()
@@ -69,31 +75,63 @@ void thd_func(int id)
 {
     int tar_cpu = id;
 
-    while(test_data.start_work == 0);        
+    uint8_t lock_mask = (uint8_t)1 << id; 
 
-    //start to work
-    int old_head;
+    printf("Target CPU id=%d, self lock mask = %.2hhx, inverse mask = %.2hhx\n", id, lock_mask, ~lock_mask);
 
-    //CAS op to find work area 
-    do{
-        old_head = test_data.head;
-    }while(!DO_CAS(&(test_data.head), old_head, old_head + thd_stride));
+    while(!force_quit){
+        //test self lock
+        while( (self_lock & lock_mask ) == 0 );
 
-    for(int i = old_head; i < (old_head + thd_stride) && i < DATA_NB; ++i){
-        test_data.data[i] = tar_cpu + 1;
+        //wait work start
+        while(test_data.start_work == 0);
+
+        //start to work
+        int old_head;
+
+        //CAS op to find work area 
+        do{
+            old_head = test_data.head;
+        }while(!DO_CAS(&(test_data.head), old_head, old_head + thd_stride));
+
+        //do work
+        for(int i = old_head; i < (old_head + thd_stride) && i < DATA_NB; ++i){
+            test_data.data[i] = tar_cpu + 1;
+        }
+
+        //CAS op to modify shared count var
+        int old_thd_count;
+
+        //encounter
+        do{
+            old_thd_count = test_data.thd_count;
+        }while(!DO_CAS(&test_data.thd_count, old_thd_count, old_thd_count + 1));
+
+        //lock self lock
+        uint8_t old_lock;
+    //    self_lock &= ~lock_mask;
+        do{
+            old_lock = self_lock; 
+        }while(!DO_CAS(&self_lock, old_lock, (uint8_t)(old_lock & ~lock_mask) ) );        
     }
-
-    //CAS op to modify shared count var
-    int old_thd_count;
-
-    do{
-        old_thd_count = test_data.thd_count;
-    }while(!DO_CAS(&test_data.thd_count, old_thd_count, old_thd_count + 1));
 }
 
 void assign_thread(std::thread ** thd, std::function<void(int)> func, int id)
 {
     *thd = new std::thread(func, id);
+}
+
+void signal_handler(int sig)
+{
+    force_quit = 1;
+
+    env_uinit();
+}
+
+void sig_init()
+{
+    force_quit = 0;
+    signal(SIGINT, signal_handler);
 }
 
 int main(int argc, char** argv)
@@ -104,25 +142,43 @@ int main(int argc, char** argv)
     pthread_mutex_init(&cond_mutex, NULL);
 
     int i = 0;
+    int test_count = 0;
 
     for(i = 0; i < MAX_THD_NB; ++i){
         assign_thread(&(thds[i]), thd_func, i);
         thread_migrate(*(thds[i]));
     }
 
-//    std::cout<<"work go"<<std::endl;
-    test_data.start_work = 1;    
+    while(1){
+        //start work
+        test_data.start_work = 1;    
 
-    while(test_data.thd_count != MAX_THD_NB);
+        //wait work over
+        while(test_data.thd_count != MAX_THD_NB);
+      //  {
+      //      printf("Wait threads over in current round, self lock = %.u\n", self_lock);
+      //  }
 
-    int error_count = 0;
+        int error_count = 0;
 
-    for(i = 0; i < DATA_NB; ++i){
-        if(test_data.data[i] == 0)
-            error_count++;
+        for(i = 0; i < DATA_NB; ++i){
+            if(test_data.data[i] == 0)
+                error_count++;
+            test_data.data[i] = 0;
+        }
+
+        printf("%d:error rate %d/%d\n", ++test_count % 10, error_count, DATA_NB);
+
+        error_count = 0;
+
+        test_data.head = 0;
+
+        test_data.thd_count = 0;
+
+        __sync_synchronize();
+
+        self_lock |= 0xFF;
     }
-
-    printf("error rate %d/%d\n", error_count, DATA_NB);
 
 #ifdef DEBUG
     for(i = 0; i < MAX_THD_NB; ++i){
